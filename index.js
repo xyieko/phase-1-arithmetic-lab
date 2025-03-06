8e6d4c53897bb320b9ee434ea214b9031bbae35c
xyieko <emmanuel.ontweka@student.moringaschool.com>
2025-03-07 00:27:02 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;  
+
+let random = Math.floor(Math.random() * 100) + 1;  
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;  
+
+let max = Math.max(10, 15, 20, 3, 5);  
+
+console.log("Multiply result:", multiply);
+console.log("Random result:", random);
+console.log("Mod result:", mod);
+console.log("Max result:", max);
